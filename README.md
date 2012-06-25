d5349114decb02990b6c4ec6d5368788cba8a4c3
Sylvain Galand <sgaland@genymobile.com>
2012-06-25 10:39:44 +0200

    Ajout de la licence et changement sdk 15 -> 14

--- README.md
+++ README.md
@@ -2,3 +2,20 @@ renderscript-particules
 =======================
 
 Tutorial simple sur la mise en place de Renderscript et l'utilisation du multithreading en étudiant le cas d'un nuage de particules
+
+Licence
+=======
+
+Copyright 2012 - Genymobile
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
--- src/com/genymobile/renderscript/particule/ParticuleView.java
+++ src/com/genymobile/renderscript/particule/ParticuleView.java
@@ -1,3 +1,18 @@
+/*
+ *   Copyright 2012 - Genymobile
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 
 package com.genymobile.renderscript.particule;
 
@@ -38,7 +53,7 @@ public class ParticuleView extends RSTextureView {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        
+
         // Définition des dimensions et bind
         particules.setWidthAndHeight(w, h);
         mRs.bindRootScript(particules.getScript());
